266e5e4a93eca50b05aa6176bc52536551112f44
mohamad-jamil <mohamadjamil03@hotmail.com>
2025-01-30 16:58:46 +0000

    Create header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,13 @@
 export default function Header() {
-  return <header>Header component</header>;
+  return (
+    <header className="bg-white flex items-center justify-between px-6 py-4 font-mono">
+      <div className="flex items-center gap-2">
+        <img src="/vite.svg" alt="logo" />
+        <h1 className="font-black text-2xl text-black">KanbanizeV2</h1>
+      </div>
+      <button className="border px-5 py-2 rounded-full cursor-pointer text-white font-bold bg-[#1868db] hover:bg-[#1868db] transition-all duration-300 ease-in-out">
+        + Add Task
+      </button>
+    </header>
+  );
 }
