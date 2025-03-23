87bda454b695f9c9caff434117b8a4ce0a4e5515
mohamad-jamil <mohamadjamil03@hotmail.com>
2025-03-23 19:08:18 +0000

    Limit task titles to one line long

--- client/src/components/Task.jsx
+++ client/src/components/Task.jsx
@@ -2,7 +2,7 @@ export default function Task({ task }) {
   return (
     <div className="w-full h-30 border-2 rounded-xl p-4 flex flex-col overflow-hidden">
       <h4 className="text-xs">ID-{task.id.toString().padStart(4, "0")}</h4>
-      <h3 className="text-lg font-bold">{task.title}</h3>
+      <h3 className="text-lg font-bold line-clamp-1">{task.title}</h3>
       <p className="text-sm font-medium line-clamp-2">{task.description}</p>
     </div>
   );
