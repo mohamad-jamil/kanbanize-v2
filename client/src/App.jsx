c2da5767c8dc43061c170e527737a0ff8320ad0c
mohamad-jamil <mohamadjamil03@hotmail.com>
2025-03-24 00:02:14 +0000

    Add functionality to create a new category/column

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,18 +3,23 @@ import { useState, useContext } from "react";
 import Header from "./components/Header";
 import Column from "./components/Column";
 import Footer from "./components/Footer";
+import AddColumnModal from "./components/AddColumnModal";
 import AddTaskModal from "./components/AddTaskModal";
 
 import { DataContext } from "./context/dataContext";
 
 function App() {
   const [addTaskModalOpen, setAddTaskModalOpen] = useState(false);
+  const [addColumnModalOpen, setAddColumnModalOpen] = useState(false);
   const { tasks, columns } = useContext(DataContext);
 
   return (
     <>
       <div className="flex flex-col h-screen">
-        <Header setAddTaskModalOpen={setAddTaskModalOpen} />
+        <Header
+          setAddColumnModalOpen={setAddColumnModalOpen}
+          setAddTaskModalOpen={setAddTaskModalOpen}
+        />
         <div className="flex flex-grow justify-center gap-8 p-8">
           {columns.map((column, index) => (
             <Column
@@ -29,6 +34,9 @@ function App() {
         </div>
         <Footer />
       </div>
+      {addColumnModalOpen && (
+        <AddColumnModal setAddColumnModalOpen={setAddColumnModalOpen} />
+      )}
       {addTaskModalOpen && (
         <AddTaskModal
           setAddTaskModalOpen={setAddTaskModalOpen}
---
+++ client/src/components/AddColumnModal.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+
+export default function AddColumnModal({ setAddColumnModalOpen }) {
+  const [category, setCategory] = useState("");
+
+  const handleSubmit = async (e) => {
+    await fetch("http://localhost:5000/columns", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        category: category,
+      }),
+    });
+
+    setAddColumnModalOpen(false);
+  };
+
+  return (
+    <div className="fixed inset-0 flex flex-col items-center justify-center bg-black/50">
+      <div className="bg-white w-1/6 rounded-md">
+        <div className="flex items-center justify-between p-4">
+          <h2 className="font-bold">Add Column</h2>
+          <button onClick={() => setAddColumnModalOpen(false)}>
+            <img
+              src="/cross.svg"
+              alt="close icon"
+              className="w-6 h-6 cursor-pointer"
+            />
+          </button>
+        </div>
+        <div className="w-full h-1 bg-black"></div>
+        <form className="flex flex-col p-4 gap-4">
+          <div className="flex flex-col">
+            <label
+              htmlFor="category"
+              className="text-sm font-medium text-gray-700"
+            >
+              Category
+            </label>
+            <input
+              type="text"
+              name="category"
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+              className="mt-1 p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+            />
+          </div>
+          <button
+            onClick={handleSubmit}
+            className="border px-5 py-2 mt-2 rounded-full cursor-pointer text-white font-bold bg-[#1868db] hover:bg-[#1868db] transition-all duration-300 ease-in-out"
+          >
+            Add column
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
